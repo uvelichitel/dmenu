ed410ce6aabea1631d36ea7bc90d96c6a5c10b3a
uvelichitel <uvelichitel@gmail.com>
2019-10-04 10:44:21 +0300

    edit manpage

--- dmenu.c
+++ dmenu.c
@@ -731,7 +731,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bcfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
 	exit(1);
 }
